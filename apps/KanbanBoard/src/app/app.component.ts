e2f1cabe116687a604d5175105cd60822aa3f8fd
ssadrian <113125604+ssadrian@users.noreply.github.com>
2022-11-29 20:16:23 +0100

    Add Tailwind and convert some css to tailwind

--- apps/KanbanBoard/src/app/app.component.ts
+++ apps/KanbanBoard/src/app/app.component.ts
@@ -6,7 +6,7 @@ import { IUser } from "./models/user/user";
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
   title: string = "Kanban Board";
@@ -115,7 +115,7 @@ export class AppComponent {
       img: "",
       alt: "",
       email: "",
-      nick: ""
+      nick: "",
     });
   }
 
