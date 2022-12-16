e1ce3a741d74b25abb7704f9cf2f30371b7e8f9f
ssadrian <113125604+ssadrian@users.noreply.github.com>
2022-12-16 23:01:13 +0100

    Complete KanbanBoard v2

--- apps/KanbanBoard/src/app/task-form/task-form.component.ts
+++ apps/KanbanBoard/src/app/task-form/task-form.component.ts
@@ -16,13 +16,13 @@ export class TaskFormComponent implements OnInit {
   @Output() onTaskSave: EventEmitter<ITask> = new EventEmitter<ITask>();
   @Output() onFormExit: EventEmitter<void> = new EventEmitter<void>();
 
-  taskFormGroup = this.fb.nonNullable.group({
+  taskFormGroup = this.fb.group({
     id: [0],
     title: ["", Validators.required],
     taskType: new FormControl<TaskType>(TaskType.TODO, [Validators.required]),
-    deadline: new FormControl(new Date()),
-    headerImage: ["https://picsum.photos/200/300"],
-    users: new FormControl(Array.of<IUser>()),
+    deadline: new FormControl<Date>(new Date()),
+    headerImage: ["https://picsum.photos/200/200"],
+    users: new FormControl<IUser[]>(Array.of<IUser>()),
   });
 
   constructor(private fb: FormBuilder) {
@@ -39,7 +39,7 @@ export class TaskFormComponent implements OnInit {
       taskType: this.task.lista,
       deadline: this.task.fechaFin,
       headerImage: this.task.img,
-      users: this.task.usuarios
+      users: [...this.task.usuarios],
     });
   }
 
@@ -65,9 +65,9 @@ export class TaskFormComponent implements OnInit {
     }
 
     let newUser: IUser = {
-      img: "https://picsum.photos/200/300",
+      img: "https://picsum.photos/200/200",
       alt: "User",
-      email: `${this.#getStringRandomNumber()}@${this.#getStringRandomNumber()}.com`,
+      email: `${ this.#getStringRandomNumber() }@${ this.#getStringRandomNumber() }.com`,
       nick: this.#getStringRandomNumber(),
     };
 
@@ -76,6 +76,28 @@ export class TaskFormComponent implements OnInit {
 
   removeUserFromTaskAtIndex(index: number): void {
     this.taskFormGroup.value.users!.splice(index, 1);
+  }
+
+  resetForm(): void {
+    this.taskFormGroup.patchValue({
+      id: this.task?.id,
+      title: this.task?.titulo,
+      taskType: this.task?.lista,
+      deadline: this.task?.fechaFin,
+      headerImage: this.task?.img,
+      users: [],
+    });
+
+    if (this.task === undefined || this.task.usuarios === undefined) {
+      return;
+    }
+
+    let newUsers: IUser[] = [];
+    for (let user of this.task.usuarios) {
+      newUsers.push(user);
+    }
+
+    this.taskFormGroup.get("users")?.setValue(newUsers);
   }
 
   exitForm(): void {
